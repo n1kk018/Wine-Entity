b24d877f2ce024e4ae1a4aa7e84095c03c12f347
n1kk018 <nicolastorero@gmail.com>
2016-10-13 16:40:56 +0200

    maj product

--- src/main/java/fr/afcepf/atod/wine/entity/Product.java
+++ src/main/java/fr/afcepf/atod/wine/entity/Product.java
@@ -176,13 +176,13 @@ public class Product implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", unique = true, nullable = false)
-	public Integer getIdProduct() {
+	public Integer getId() {
 		return id;
 	}
 
 
 
-	public void setIdProduct(Integer id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
